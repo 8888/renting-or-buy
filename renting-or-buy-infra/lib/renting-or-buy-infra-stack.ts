2ab628eb0f6cb4eb54c0f2775d62367f10ebc4e6
8888 <leecostello1@gmail.com>
2022-02-21 17:38:11 -0500

    establish CDK infra for deployment

---
+++ renting-or-buy-infra/lib/renting-or-buy-infra-stack.ts
@@ -0,0 +1,62 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { CloudFrontAllowedMethods, CloudFrontWebDistribution, OriginAccessIdentity } from 'aws-cdk-lib/aws-cloudfront';
+import { BlockPublicAccess, Bucket } from 'aws-cdk-lib/aws-s3';
+import { CanonicalUserPrincipal, PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
+import { AaaaRecord, PublicHostedZone, RecordTarget } from 'aws-cdk-lib/aws-route53';
+import { CloudFrontTarget } from 'aws-cdk-lib/aws-route53-targets';
+
+export class RentingOrBuyInfraStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const cloudfrontOAI = new OriginAccessIdentity(this, 'cloudfront-OAI', {
+      comment: `OAI for ${id}`,
+    });
+
+    const hostingBucket = new Bucket(this, 'RentingOrBuyBucket', {
+      websiteIndexDocument: 'index.html',
+      publicReadAccess: false,
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+    });
+
+    hostingBucket.addToResourcePolicy(new PolicyStatement({
+      actions: [ 's3:GetObject' ],
+      resources: [ hostingBucket.arnForObjects('*') ],
+      principals: [ new CanonicalUserPrincipal(cloudfrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId, )]
+    }));
+
+    const distribution = new CloudFrontWebDistribution(this, 'SiteDistribution', {
+      originConfigs: [
+        {
+          s3OriginSource: {
+            s3BucketSource: hostingBucket,
+            originAccessIdentity: cloudfrontOAI,
+          },
+          behaviors: [{
+            isDefaultBehavior: true,
+            compress: true,
+            allowedMethods: CloudFrontAllowedMethods.GET_HEAD_OPTIONS,
+          }],
+        }
+      ],
+    });
+
+    new BucketDeployment(this, 'DeployWithInvalidation', {
+      sources: [ Source.asset('../src') ],
+      destinationBucket: hostingBucket,
+      distribution,
+      distributionPaths: ['/*'],
+    });
+
+    const hostedZone = new PublicHostedZone(this, 'HostedZone', {
+      zoneName: 'rentingorbuy.com',
+    });
+
+    new AaaaRecord(this, 'Alias', {
+      zone: hostedZone,
+      target: RecordTarget.fromAlias(new CloudFrontTarget(distribution)),
+    });
+  }
+}
---
+++ renting-or-buy-infra/test/renting-or-buy-infra.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as RentingOrBuyInfra from '../lib/renting-or-buy-infra-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/renting-or-buy-infra-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new RentingOrBuyInfra.RentingOrBuyInfraStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
